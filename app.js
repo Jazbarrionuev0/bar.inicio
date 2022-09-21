38044dba2c4a1f370a5ee7f7f9e63b6c126398fc
Jazmín <100377765+Jazbarrionuev0@users.noreply.github.com>
2022-09-21 15:02:24 -0300

    falta arreglar el carrusel que no se posiciona bien

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const myCarouselElement = document.querySelector('.carousel')
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 5000,
+  wrap: false
+})
 
 let wsp = document.querySelector(".wsp");
 let nombre = document.querySelector(".nombre");
@@ -5,24 +10,14 @@ let dia = document.querySelector(".dia");
 let hora = document.querySelector(".hora");
 let guardar = document.querySelector(".guardar");
 
-let refrescar = 0;
-
-
 guardar.addEventListener("click",reservarEvento);
 
 function reservarEvento(){
 
-    if(refrescar == 0){
-        refrescar = 1;
-        wsp.outerHTML = `<a target=\"_blank\" class=\"wsp\" href=\"https://api.whatsapp.com/send?phone=3413157248&amp;text=Hola,quiero hacer una reserva a nombre de `+ nombre.value +` el dia `+ dia.value +` a las `+ hora.value +` \">Reserva</a>`
+    wsp.outerHTML = `<a target=\"_blank\" class=\"wsp\" href=\"https://api.whatsapp.com/send?phone=3413157248&amp;text=Hola,quiero hacer una reserva a nombre de `+ nombre.value +` el dia `+ dia.value +` a las `+ hora.value +` \">Reserva</a>`
 
-        wsp.classList.remove("ocultar");
-    
-        console.log(wsp.outerHTML);
-    }else{
-        location. reload()
-    }
-
+    wsp.classList.remove("ocultar");
+    guardar.classList.add("ocultar");
 }
 
 
