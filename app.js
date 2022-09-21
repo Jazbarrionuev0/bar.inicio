c62b2cdcf9dc8e407b19b599748f76ed72d4b412
Jazmín <100377765+Jazbarrionuev0@users.noreply.github.com>
2022-09-21 13:34:41 -0300

    falta el carrusel

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 
-
 let wsp = document.querySelector(".wsp");
 let nombre = document.querySelector(".nombre");
 let dia = document.querySelector(".dia");
 let hora = document.querySelector(".hora");
 let guardar = document.querySelector(".guardar");
 
+let refrescar = 0;
+
 
 guardar.addEventListener("click",reservarEvento);
 
 function reservarEvento(){
 
-    wsp.outerHTML = `<a target=\"_blank\" class=\"wsp\" href=\"https://api.whatsapp.com/send?phone=3413157248&amp;text=Hola,quiero hacer una reserva a nombre de `+ nombre.value +` el dia `+ dia.value +` a las `+ hora.value +` \">Reserva</a>`
+    if(refrescar == 0){
+        refrescar = 1;
+        wsp.outerHTML = `<a target=\"_blank\" class=\"wsp\" href=\"https://api.whatsapp.com/send?phone=3413157248&amp;text=Hola,quiero hacer una reserva a nombre de `+ nombre.value +` el dia `+ dia.value +` a las `+ hora.value +` \">Reserva</a>`
 
-    wsp.classList.remove("ocultar");
-    guardar.classList.add("ocultar");
+        wsp.classList.remove("ocultar");
+    
+        console.log(wsp.outerHTML);
+    }else{
+        location. reload()
+    }
+
 }
 
 
